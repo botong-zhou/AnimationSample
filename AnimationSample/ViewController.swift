606c489a53555f0e6d88a456f185cc323283e2bb
bin1991 <bin1991@bin1991deiMac.local>
2015-07-07 13:36:41 +0700

    Initial Commit

---
+++ AnimationSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationSample
+//
+//  Created by bin1991 on 15/7/7.
+//  Copyright (c) 2015年 bin1991. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
